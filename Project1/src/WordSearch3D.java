0199cac67adc399a7e122c53752c80e0df02e1a3
Jacobvs <jacobvs2@gmail.com>
2019-10-30 16:58:59 -0400

    Bugfixes: Fix infinite loop in make method for boards with more words than space

--- Project1/src/WordSearch3D.java
+++ Project1/src/WordSearch3D.java
@@ -147,12 +147,13 @@ public class WordSearch3D {
         char[][][] board = new char[sizeX][sizeY][sizeZ]; //make new board
         ArrayList<String> wordsToPlace = new ArrayList<>(Arrays.asList(words));
 
-        for (int i = 1; i < 1000; i++) {
+        for (int i = 1; i < 1000; i++) { // 1000 tries to make a completed board
             while(true){
                 int w = new Random().nextInt(wordsToPlace.size());
                 String word = wordsToPlace.get(w);
 
                 board = placeWord(board, word);
+                System.out.println(Arrays.deepToString(board));
                 if(board != null){
                     if(wordsToPlace.size() == 1) {
                         System.out.println("Completed board:");
@@ -162,6 +163,8 @@ public class WordSearch3D {
                         System.out.println(Arrays.deepToString(board));
                         return board;
                     }
+                    else if(checkFullBoard(board))
+                        return null;
                     else
                         wordsToPlace.remove(w);
                 }
@@ -180,7 +183,6 @@ public class WordSearch3D {
     }
 
     public char[][][] placeWord(char[][][] board, String word) {
-
         for (int i = 0; i < 1000; i++) {
             int x = 0, y = 0, z = 0;
             while (board[x][y][z] != '\0') {
@@ -211,6 +213,7 @@ public class WordSearch3D {
                 }
             }
         }
+        System.out.println("word is null");
         return null;
     }
 
@@ -225,6 +228,19 @@ public class WordSearch3D {
             }
         }
         return board;
+    }
+
+    public boolean checkFullBoard (char[][][] board){
+        for (int i = 0; i < board.length; i++) {
+            for (int j = 0; j < board[i].length; j++) {
+                for (int k = 0; k < board[i][j].length; k++) {
+                    if(board[i][j][k] == '\0'){
+                        return false;
+                    }
+                }
+            }
+        }
+        return true;
     }
 
     /**
