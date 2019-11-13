66eaca940b80546feee74397d48c066c8bcd21d5
Will Burke <wgburke@wpi.edu>
2019-11-12 20:50:55 -0500

    Implemented template BFS search

--- Project3/src/GraphSearchEngineImpl.java
+++ Project3/src/GraphSearchEngineImpl.java
@@ -1,9 +1,25 @@
-import java.util.List;
+import java.util.*;
 
 public class GraphSearchEngineImpl implements GraphSearchEngine {
 
     @Override
     public List<Node> findShortestPath(Node s, Node t) {
+        Queue<Node> _nodesToVisit = new ArrayDeque<>();
+        HashSet<Node> _visitedNodes = new HashSet<>();
+        ArrayList<Node> _pathOfNodes = new ArrayList<>();
+
+        _nodesToVisit.add(s); // Adds first node to nodesToVisit queue
+
+        while(_nodesToVisit.size() > 0){ // While nodesToVisit is not empty
+            Node temp = _nodesToVisit.remove(); // sets temp Node to first node in queue and removes
+            _visitedNodes.add(temp); // Adds removed Node to list of visited nodes
+            for(Node neighbor: temp.getNeighbors()){ // For all the neighbors of temp, add them to nodesToVisit if unseen
+                if(!_nodesToVisit.contains(neighbor) && !_visitedNodes.contains(neighbor)){
+                    _nodesToVisit.add(neighbor);
+                }
+            }
+        }
+
         return null;
     }
 
